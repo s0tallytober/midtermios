5350e32750a72b2777634ea6ea53006b0c1bbf23
venkateshk <s0tallytober>
2018-10-18 19:15:25 -0400

    Initial Commit

---
+++ MidtermtestShoppinglist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MidtermtestShoppinglist
+//
+//  Created by Venkatesh K on Saka 1940-07-26.
+//  Copyright © 1940 Saka Venkatesh K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
