56538f477e6eeb66798052651ab049ba7c408032
venkateshk <s0tallytober>
2018-10-18 21:39:25 -0400

    UI scaled

--- MidtermtestShoppinglist/ViewController.swift
+++ MidtermtestShoppinglist/ViewController.swift
@@ -12,14 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        item1.text = "1"
+        item2.text = "1"
+        item3.text = "1"
+        item4.text = "1"
+        item5.text = "1"
+        item6.text = "1"
+        listname.text = "1"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBOutlet weak var listname: UITextField!
+    @IBOutlet weak var item1: UITextField!
+    @IBOutlet weak var item2: UITextField!
+    @IBOutlet weak var item3: UITextField!
+    @IBOutlet weak var item4: UITextField!
+    @IBOutlet weak var item5: UITextField!
+    @IBOutlet weak var item6: UITextField!
+    @IBOutlet weak var quantity1: UIStepper!
+    @IBOutlet weak var quantity2: UIStepper!
+    @IBOutlet weak var quantity3: UIStepper!
+    @IBOutlet weak var quantity4: UIStepper!
+    @IBOutlet weak var quantity5: UIStepper!
+    @IBOutlet weak var quantity6: UIStepper!
+    @IBOutlet weak var qbox1: UITextField!
+    @IBOutlet weak var qbox2: UITextField!
+    @IBOutlet weak var qbox3: UITextField!
+    @IBOutlet weak var qbox5: UITextField!
+    @IBOutlet weak var qbox6: UITextField!
+    
+    @IBOutlet weak var qbox4: UITextField!
+    @IBOutlet weak var save: UIButton!
+    @IBOutlet weak var cancel: UIButton!
 }
+
 
